3d2ae00551bdf8a4f4a11002ca4486bb82316f58
danvb112 <danielbonasser@gmail.com>
2026-01-06 08:36:45 -0300

    Get webhook by id

--- web/src/routes/webhooks.$id.tsx
+++ web/src/routes/webhooks.$id.tsx
@@ -3,43 +3,57 @@ import { WebhookDetailHeader } from '../components/webhook-detail-header'
 import { SectionTitle } from '../components/section-title'
 import { SectionDataTable } from '../components/section-data-table'
 import { CodeBlock } from '../components/ui/code-block'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { webhookDetailsSchema } from '../http/schemas/webhooks'
 
 export const Route = createFileRoute('/webhooks/$id')({
-  component: RouteComponent,
+    component: RouteComponent,
 })
 
 function RouteComponent() {
-  const overviewData = [
-    {
-      key: 'Method',
-      value: 'POST',
-    },
-    {
-      key: 'Status Code',
-      value: '200',
-    },
-    {
-      key: 'Content-Type',
-      value: 'application/json',
-    },
-    {
-      key: 'Content-Length',
-      value: '342 bytes',
-    },
-  ]
+    const { id } = Route.useParams()
 
-  return (
-    <div className="flex h-full flex-col">
-      <WebhookDetailHeader />
-      <div className="flex-1 overflow-y-auto">
-        <div className=" space-y-6 p-6">
-          <div className="space-y-4">
-            <SectionTitle>Request Overview</SectionTitle>
-            <SectionDataTable data={overviewData} />
-            <CodeBlock code={JSON.stringify(overviewData, null, 2)} />
-          </div>
+    const { data } = useSuspenseQuery({
+        queryKey: ['webhook', id],
+        queryFn: async () => {
+            const response = await fetch(`http://localhost:3333/api/webhooks/${id}`)
+            const data = await response.json()
+
+            return webhookDetailsSchema.parse(data)
+        },
+    })
+
+    const overviewData = [
+        {
+            key: 'Method',
+            value: data.method,
+        },
+        {
+            key: 'Status Code',
+            value: String(data.statusCode),
+        },
+        {
+            key: 'Content-Type',
+            value: data.contentType || 'application/json',
+        },
+        {
+            key: 'Content-Length',
+            value: `${data.contentLength || 0} bytes`,
+        },
+    ]
+
+    return (
+        <div className="flex h-full flex-col">
+            <WebhookDetailHeader />
+            <div className="flex-1 overflow-y-auto">
+                <div className=" space-y-6 p-6">
+                    <div className="space-y-4">
+                        <SectionTitle>Request Overview</SectionTitle>
+                        <SectionDataTable data={overviewData} />
+                        <CodeBlock code={JSON.stringify(overviewData, null, 2)} />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
