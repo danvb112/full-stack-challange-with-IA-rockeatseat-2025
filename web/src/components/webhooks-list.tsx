bde147416d2ecaa19b0851994d7884076dc3de21
danvb112 <danielbonasser@gmail.com>
2026-01-06 07:51:40 -0300

    Use WebhookListItem and create webhook schema to use in the query function

--- web/src/components/webhooks-list.tsx
+++ web/src/components/webhooks-list.tsx
@@ -1,31 +1,26 @@
-import { Link } from '@tanstack/react-router'
-import { IconButton } from './ui/icon-button'
-import { Trash2Icon } from 'lucide-react'
-import { Checkbox } from './ui/checkbox'
+import { useQuery } from '@tanstack/react-query'
+import { webhookListSchema } from '../http/schemas/webhooks'
+import { WebhookListItem } from './webhook-list-item'
 
 export function WebhookList() {
-  return (
-    <div className="group rounded-lg transition-colors duration-150 hover:bg-zinc-700/30">
-      <div className="flex items-start gap-3 px-4 py-2.5">
-        <Checkbox />
-        <Link to="/" className="flex flex-1 min-w-0 items-start gap-3">
-          <span className="w-12 shrink-0 font-mono text-xs font-semibold text-zinc-300 text-right">
-            POST
-          </span>
-          <div className="flex-1 min-w-0">
-            <p className="truncate text-xs text-zinc-200 leading-tight font-mono">
-              /video/status
-            </p>
-            <p className="text-xs text-zinc-500 font-medium mt-1">
-              1 minute ago
-            </p>
-          </div>
-        </Link>
+  const { data, isLoading } = useQuery({
+    queryKey: ['webhooks'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/api/webhooks')
+      const data = await response.json()
 
-        <IconButton
-          icon={<Trash2Icon className="size-3.5 text-zinc-400" />}
-          className="opacity-0 transition-opacity group-hover:opacity-100"
-        />
+      return webhookListSchema.parse(data)
+    },
+  })
+
+  return (
+    <div className='flex-1 overflow-y-auto'>
+      <div className='space-y-1 p-2'>
+        <WebhookListItem />
+        <WebhookListItem />
+        <WebhookListItem />
+        <WebhookListItem />
+        <WebhookListItem />
       </div>
     </div>
   )
